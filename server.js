2e388c4a389067b869cd997e212a92053a1ed487
Aizat Faiz <aizat.faiz@gmail.com>
2017-06-22 21:09:42 -0700

    nit

--- server.js
+++ server.js
@@ -2,34 +2,31 @@
 
 import express from 'express';
 import path from 'path';
-import webpack from 'webpack';
-import WebpackDevServer from 'webpack-dev-server';
 
-const APP_PORT = process.env.NODE_ENV === 'production' && process.env.PORT
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
+const APP_PORT = PRODUCTION && process.env.PORT
   ? process.env.PORT
   : 3000;
 
 // Serve the Relay app
-const compiler = webpack({
-  entry: path.resolve(__dirname, 'js', 'app.js'),
-  module: {
-    loaders: [
-      {
-        exclude: /node_modules/,
-        loader: 'babel',
-        test: /\.js$/,
-      },
-      { test: /\.json$/, loader: 'json-loader' }
-    ],
-  },
-  output: {filename: 'app.js', path: '/'},
-});
+let app = null;
 
-const app = new WebpackDevServer(compiler, {
-  contentBase: '/public/',
-  publicPath: '/js/',
-  stats: {colors: true},
-});
+if (PRODUCTION) {
+  app = express();
+} else {
+  const webpack = require('webpack');
+  const WebpackDevServer = require('webpack-dev-server');
+
+  const config = require('./webpack.config.js');
+  const compiler = webpack(config);
+  
+  app = new WebpackDevServer(compiler, {
+    contentBase: '/public/',
+    publicPath: '/js/',
+    stats: {colors: true},
+  });
+}
 
 // Serve static resources
 app.use('/', express.static(path.resolve(__dirname, 'public')));
